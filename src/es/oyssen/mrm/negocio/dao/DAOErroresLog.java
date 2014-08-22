b5da4425d27860ea94be001f052647f4befba0a1
am.aroyo@gmail.com <am.aroyo@gmail.com@1f435f5d-69a8-1162-80f1-c6e45ea72d58>
2014-08-22 17:40:17 +0000

    errores log service

---
+++ src/es/oyssen/mrm/negocio/dao/DAOErroresLog.java
@@ -0,0 +1,27 @@
+package es.oyssen.mrm.negocio.dao;
+
+import java.util.List;
+
+import es.oyssen.mrm.negocio.dao.exceptions.DAODeleteException;
+import es.oyssen.mrm.negocio.dao.exceptions.DAOException;
+import es.oyssen.mrm.negocio.dao.exceptions.DAOInsertException;
+import es.oyssen.mrm.negocio.dao.exceptions.DAOUpdateException;
+import es.oyssen.mrm.negocio.vo.AnexoVO;
+import es.oyssen.mrm.negocio.vo.ErrorLogVO;
+
+public interface DAOErroresLog {
+
+	public void insert(ErrorLogVO error) throws DAOException, DAOInsertException;
+	
+	public void update(ErrorLogVO error) throws DAOException, DAOUpdateException;
+	
+	public void delete(ErrorLogVO error) throws DAOException, DAODeleteException;
+		
+	public List<ErrorLogVO> findAll(ErrorLogVO error) throws DAOException;
+	
+	public ErrorLogVO findById(ErrorLogVO error) throws DAOException;
+	
+	
+	
+
+}
---
+++ src/es/oyssen/mrm/negocio/services/ErroresLogService.java
@@ -0,0 +1,22 @@
+package es.oyssen.mrm.negocio.services;
+
+import java.util.List;
+
+import es.oyssen.mrm.negocio.dao.exceptions.DAOException;
+import es.oyssen.mrm.negocio.exceptions.ServiceException;
+import es.oyssen.mrm.negocio.vo.AnexoVO;
+import es.oyssen.mrm.negocio.vo.ErrorLogVO;
+
+public interface ErroresLogService {
+	
+	public void insert(ErrorLogVO error) throws ServiceException, DAOException;
+	
+	public void update(ErrorLogVO error) throws ServiceException, DAOException;
+	
+	public void delete(ErrorLogVO error) throws ServiceException, DAOException;
+	
+	public List<ErrorLogVO> findAll(ErrorLogVO error) throws ServiceException;
+	
+	public ErrorLogVO findById(ErrorLogVO error) throws ServiceException;
+
+}
